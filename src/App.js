7c3d8516961bcfaa6cf08272a633842f79e1145c
Tarjei Sandsnes <tarjeisandsnes@Tarjeis-MacBook-Air.local>
2023-01-24 17:42:00 +0100

    added baseline changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import TextField from "@mui/material/TextField";
+import SelectSearch from 'react-select-search';
+import 'react-select-search/style.css';
 
-function App() {
+let data = require('./data.json')
+//Translate author table
+function authorToDict(data) {
+  function transferToDict(id) {
+    var dictionary = data['data'][id];
+    var id_int = parseInt(id)+1
+    var birth = dictionary['Birth'];
+    var death = dictionary['Death'];
+    var deathString
+    if (death<0) {deathString= Math.abs(death)+" BC"}
+    else{deathString = death+ " AD"}    
+    return {
+      id:id_int,
+      name: dictionary['Name'],
+      position: checkNull(dictionary['Position']),
+      birth: checkNull(birth),
+      death: checkNull(death),
+      floruit: checkNull(dictionary['Floruit']),
+      country: checkNull(dictionary['Country']),
+      city: checkNull(dictionary['City/Region']),
+      deathString:deathString,
+    }
+}
+  var list = [];
+  for (var n in data['data']) {list.push(transferToDict(n))}
+  return list
+}
+let listOfAuthors = authorToDict(data)
+
+//translate nulls to unknown
+function checkNull(data){
+  if(data === null) {data = "unknown"}
+  return data}
+
+//Options for dropdown
+const options = [
+  {name: 'Swedish', value: 'sv'},
+  {name: 'English', value: 'en'},
+];
+
+
+/*Building a frame for an author:
+  Author name tr -> later a popdown
+  Birth & Location tr 
+  Death tr
+  (floruit if birth/death does not exist)
+  Position tr
+  BOOK COMPONENT
+*/
+
+function AuthorSearch (props) {
+    return (
+      <tr>
+        <td>
+        <a href = {"https://www.google.com/search?q="+props.value.name}>
+          <button>{"#"+props.value.id+": "+props.value.name.split(",")[0] + ", " + props.value.position +
+            " ("+props.value.birth+"-"+props.value.death/*+", fl. " + props.value.floruit*/+")" }</button>
+          </a>
+        </td>
+      </tr>
+		)
+	
+  }
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "marcus",
+      data: listOfAuthors.slice(1,1),
+      dataToShow: true,
+      searchResults: "",
+  }
+  };
+
+  authorSearch = event => {
+    var data = listOfAuthors;
+    var search = this.state.search.toLowerCase();
+    search = search.split(" ")
+    for (let i = 0; i<search.length;i++){
+      data = data.filter(
+      e=> 
+      (e.name+e.position+e.country+e.city).toLowerCase()
+      .includes(search[i])
+      )}
+    this.setState({data:data})
+    }
+    
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className = "search">
+          <TextField
+            id="outlined-basic"
+            onChange={e => {
+              this.setState({search:e.target.value});
+              this.authorSearch();
+              }}
+            variant="outlined"
+            //fullwidth
+            label="Search"
+          />
+          {"# search results: " + this.state.data.length}
+          <table>
+          <tbody>
+          {this.state.data.map((author) => <AuthorSearch value = {author} key={author.id}
+          
+          />)}
+          </tbody>
+          </table>
+        </div>
     </div>
   );
+}
 }
 
 export default App;
